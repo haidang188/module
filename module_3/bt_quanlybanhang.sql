d8389c13a855217c78effc7eb34bfaebe139c798
haidang188 <nguyenhahaidangit@gmail.com>
2026-03-12 15:24:57 +0700

    ok

---
+++ module_3/bt_quanlybanhang.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE QuanLyBanHang;
+USE QuanLyBanHang;
+
+CREATE TABLE Customer (
+cID INT PRIMARY KEY,
+cName VARCHAR(50),
+cAge INT
+);
+
+CREATE TABLE `order` (
+oID INT PRIMARY KEY,
+cID INT,
+oDate DATE,
+oTotalPrice DECIMAL(10,2),
+FOREIGN KEY (cID) REFERENCES Customer(cID)
+);
+
+CREATE TABLE Product (
+pID INT PRIMARY KEY,
+pName VARCHAR(50),
+pPrice DECIMAL(10,2)
+);
+
+CREATE TABLE OrderDetail (
+oID INT,
+pID INT,
+odQTY INT,
+PRIMARY KEY (oID, pID),
+FOREIGN KEY (oID) REFERENCES `order` (oID),
+FOREIGN KEY (pID) REFERENCES Product (pID)
+);
